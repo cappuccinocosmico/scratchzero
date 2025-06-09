487844c3aefa9ac269bc8a14b2956fe753fb87c9
Nicole Venner <nvenner@protonmail.ch>
2025-06-09 11:45:39 -0600

    stop this for now

--- src/mnist/fixed_tensor.rs
+++ src/mnist/fixed_tensor.rs
@@ -1,22 +1,92 @@
+use const_for::const_for;
+use rand::random_range;
+
 type DimSizeType = usize;
 type flt = f32;
 
-const fn dims_product(dims: &[usize]) -> usize {
+const fn dims_size<const n: usize>(dims: [usize; n]) -> usize {
     let mut res = 0;
-    for i in dims {
-        res = res + i;
-    }
+    const_for!( i in 0..dims.len() => {
+        res = res * dims[i];
+    });
     res
 }
 
-struct FixedTensor<const DIM: usize, const DIMS: [usize; DIM]> {
-    values: [flt; dims_product(&DIMS)],
+const fn matsize(a: usize, b: usize) -> usize {
+    return a * b;
 }
 
-impl<const DIM: usize, const DIMS: [usize; DIM]> FixedTensor<DIM, DIMS> {
-    const fn zeros() -> Box<Self> {
-        Box::new(Self {
-            values: [0.0; dims_product(&DIMS)],
-        })
+// I want you to get around this by creating a macro: define_tensor(num)
+// That will make a generic tensor of a specific dimension like so:
+// define_tensor_type!(2)
+// expands to:
+struct Tensor_2<const A: usize, const B: usize>
+where
+    [flt; A * B]: Sized,
+{
+    values: [flt; A * B],
+}
+impl<const A: usize, const B: usize> Tensor_2<A, B>
+where
+    [flt; A * B]: Sized,
+{
+    const SIZE: usize = A * B;
+
+    fn zeros() -> Self {
+        Self {
+            values: [0.0; A * B],
+        }
+    }
+
+    fn random() -> Self {
+        let mut values = [0.0; A * B];
+        for i in 0..values.len() {
+            values[i] = random_range(-1.0..1.0)
+        }
+        return Self { values };
+    }
+    fn add(&self, other: &Self) -> Self {
+        let mut values = [0.0; A * B];
+        for i in 0..values.len() {
+            values[i] = self.values[i] + other.values[i]
+        }
+        return Self { values };
     }
 }
+// For any integer from 1 to 10.
+
+// In order to get around the bugs I had for the previous implementation
+// 1. the type of const parameters must not depend on other generic parameters
+//    the type must not depend on the parameter `DIM` [E0770]
+// 2. `[usize; DIM]` is forbidden as the type of a const generic parameter
+//    the only supported types are integers, `bool`, and `char`
+// struct FixedTensor<const DIM: usize, const DIMS: [usize; DIM]> {
+//     values: [flt; dims_size(&DIMS)],
+// }
+//
+// impl<const DIM: usize, const DIMS: [usize; DIM]> FixedTensor<DIM, DIMS> {
+//     const fn size() -> usize {
+//         dims_size(&DIMS)
+//     }
+//     fn zeros() -> Self {
+//         Self {
+//             values: [0.0; dims_size(&DIMS)],
+//         }
+//     }
+//
+//     fn random() -> Self {
+//         let mut values: [flt; dims_size(&DIMS)];
+//         for i in 0..values.len() {
+//             values[i] = random_range(-1.0..1.0)
+//         }
+//         return Self { values };
+//     }
+//
+//     fn add(&self, other: &Self) -> Self {
+//         let mut values: [flt; dims_size(&DIMS)];
+//         for i in 0..values.len() {
+//             values[i] = self.values[i] + other.values[i]
+//         }
+//         return Self { values };
+//     }
+// }
--- src/mnist/layers/linear.rs
+++ src/mnist/layers/linear.rs
@@ -1,5 +1,4 @@
 use crate::mnist::{
-    field::Field,
     module::{ModConfig, Module},
     tensor::Tensor,
 };
@@ -31,85 +30,85 @@ impl ModConfig for Linear {
     type Cache = LinearCache;
 }
 
-impl Module for Linear {
-    fn forward(&self, input: &Self::Input) -> (Self::Output, Self::Cache) {
-        let in_features = input.shape()[1];
-        let out_features = self.b.shape()[0];
-        let mut out = Tensor::<1>::zeros([out_features]);
-        // out[b, j] = sum_k input[b, k] * w[j, k] + b[j]
-        for j in 0..out_features {
-            let mut sum = 0.0;
-            for k in 0..in_features {
-                sum += input.data()[k] * self.w.data()[j * in_features + k];
-            }
-            sum += self.b.data()[j];
-            out.data_mut()[j] = sum;
-        }
-        (
-            out,
-            LinearCache {
-                input: input.clone(),
-            },
-        )
-    }
-
-    fn backward(
-        &self,
-        grad_output: &Self::Output,
-        cache: &Self::Cache,
-    ) -> (Self::Input, Self::Param) {
-        let in_features = input.shape()[1];
-        let out_features = self.b.shape()[0];
-        let mut out = Tensor::<1>::zeros([out_features]);
-        // out[b, j] = sum_k input[b, k] * w[j, k] + b[j]
-        for j in 0..out_features {
-            let mut sum = 0.0;
-            for k in 0..in_features {
-                sum += input.data()[k] * self.w.data()[j * in_features + k];
-            }
-            sum += self.b.data()[j];
-            out.data_mut()[j] = sum;
-        }
-        (
-            out,
-            LinearCache {
-                input: input.clone(),
-            },
-        )
-    }
-        let batch = grad_output.shape()[0];
-        let in_features = cache.input.shape[1];
-        let out_features = grad_output.shape[1];
-        // dW shape [out_features, in_features]
-        let mut dW = Tensor::<2>::zeros([out_features, in_features]);
-        // dB shape [out_features]
-        let mut dB = Tensor::<1>::zeros([out_features]);
-        // dX shape [batch, in_features]
-        let mut dX = Tensor::<2>::zeros([batch, in_features]);
-
-        for b_idx in 0..batch {
-            for j in 0..out_features {
-                let go = grad_output.data()[b_idx * out_features + j];
-                dB.data_mut()[j] += go;
-                for k in 0..in_features {
-                    dW.data_mut()[j * in_features + k] +=
-                        go * cache.input.data()[b_idx * in_features + k];
-                    dX.data_mut()[b_idx * in_features + k] +=
-                        go * self.w.value.data()[j * in_features + k];
-                }
-            }
-        }
-        (dX, (dW, dB))
-    }
-
-    fn update(&mut self, param_grad: &Self::Param, lr: f32) {
-        let (ref dW, ref dB) = *param_grad;
-        // SGD update
-        for (w_val, dw) in self.w.value.data_mut().iter_mut().zip(dW.data().iter()) {
-            *w_val -= lr * dw;
-        }
-        for (b_val, db) in self.b.value.data_mut().iter_mut().zip(dB.data().iter()) {
-            *b_val -= lr * db;
-        }
-    }
-}
+// impl Module for Linear {
+//     fn forward(&self, input: &Self::Input) -> (Self::Output, Self::Cache) {
+//         let in_features = input.shape()[1];
+//         let out_features = self.b.shape()[0];
+//         let mut out = Tensor::<1>::zeros([out_features]);
+//         // out[b, j] = sum_k input[b, k] * w[j, k] + b[j]
+//         for j in 0..out_features {
+//             let mut sum = 0.0;
+//             for k in 0..in_features {
+//                 sum += input.data()[k] * self.w.data()[j * in_features + k];
+//             }
+//             sum += self.b.data()[j];
+//             out.data_mut()[j] = sum;
+//         }
+//         (
+//             out,
+//             LinearCache {
+//                 input: input.clone(),
+//             },
+//         )
+//     }
+//
+//     fn backward(
+//         &self,
+//         grad_output: &Self::Output,
+//         cache: &Self::Cache,
+//     ) -> (Self::Input, Self::Param) {
+//         let in_features = input.shape()[1];
+//         let out_features = self.b.shape()[0];
+//         let mut out = Tensor::<1>::zeros([out_features]);
+//         // out[b, j] = sum_k input[b, k] * w[j, k] + b[j]
+//         for j in 0..out_features {
+//             let mut sum = 0.0;
+//             for k in 0..in_features {
+//                 sum += input.data()[k] * self.w.data()[j * in_features + k];
+//             }
+//             sum += self.b.data()[j];
+//             out.data_mut()[j] = sum;
+//         }
+//         (
+//             out,
+//             LinearCache {
+//                 input: input.clone(),
+//             },
+//         )
+//     }
+//         let batch = grad_output.shape()[0];
+//         let in_features = cache.input.shape[1];
+//         let out_features = grad_output.shape[1];
+//         // dW shape [out_features, in_features]
+//         let mut dW = Tensor::<2>::zeros([out_features, in_features]);
+//         // dB shape [out_features]
+//         let mut dB = Tensor::<1>::zeros([out_features]);
+//         // dX shape [batch, in_features]
+//         let mut dX = Tensor::<2>::zeros([batch, in_features]);
+//
+//         for b_idx in 0..batch {
+//             for j in 0..out_features {
+//                 let go = grad_output.data()[b_idx * out_features + j];
+//                 dB.data_mut()[j] += go;
+//                 for k in 0..in_features {
+//                     dW.data_mut()[j * in_features + k] +=
+//                         go * cache.input.data()[b_idx * in_features + k];
+//                     dX.data_mut()[b_idx * in_features + k] +=
+//                         go * self.w.value.data()[j * in_features + k];
+//                 }
+//             }
+//         }
+//         (dX, (dW, dB))
+//     }
+//
+//     fn update(&mut self, param_grad: &Self::Param, lr: f32) {
+//         let (ref dW, ref dB) = *param_grad;
+//         // SGD update
+//         for (w_val, dw) in self.w.value.data_mut().iter_mut().zip(dW.data().iter()) {
+//             *w_val -= lr * dw;
+//         }
+//         for (b_val, db) in self.b.value.data_mut().iter_mut().zip(dB.data().iter()) {
+//             *b_val -= lr * db;
+//         }
+//     }
+// }
